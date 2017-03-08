650a046c8dc6bbd983d4471c9d149dcd6db21109
ghesmoune <riadfairouz@gmail.com>
2017-03-08 00:05:20 -0500

    exicter une select sur la table etudiant

--- src/test/Test.java
+++ src/test/Test.java
@@ -7,9 +7,10 @@ package test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.sql.Statement;
 
 /**
  *
@@ -30,10 +31,29 @@ Connection connexion = null;
             Class.forName("com.mysql.jdbc.Driver");
             connexion = DriverManager.getConnection(url,user, password);
              System.out.println("Connection etablis");
+             Statement state= connexion.createStatement();
+             ResultSet result= state.executeQuery("SELECT * FROM etudiant");
+             ResultSetMetaData  metaData = result.getMetaData();
+             System.out.println("---------------------------------------------------------");
+             for(int i=1;i<=metaData.getColumnCount();i++){
+                 System.out.print("\t"+metaData.getColumnName(i)+"\t|");
+             }
+            System.out.println("\n---------------------------------------------------");
+            while(result.next()){
+             for(int i=1;i<=metaData.getColumnCount();i++){
+                 System.out.print("\t"+result.getObject(i)+"\t|");
+             }
+             System.out.println("\n-------------------------------------------------------");
+            }
+             result.close();
+             state.close();   
+             connexion.close();
         } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
             System.out.println("Le pilote jdbc introuvable");
             
         } catch (SQLException ex) {
+            ex.printStackTrace();
             System.out.println("Les parametres de connnection sont invalide");
         }
     }
